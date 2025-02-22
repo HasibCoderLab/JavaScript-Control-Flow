58ae6b426268ab0be8fdc41ab97e689dae8a7367
Hasib Hasan <mohammodhasibhasan@gmail.com>
2025-02-22 22:53:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// ========================
+//      ***** Practice Problem 1 *****
+// ======================== 
+
+const chekeLength = (name) =>{
+    if(name.length > 6){
+        return "Accept your idea"
+    }
+    else{
+        return"Please change your idea"
+    }
+}
+console.log(chekeLength("JavaScript"))
+// Output:Accept your idea
+console.log(chekeLength("HTML")) 
+// Output: Please change your idea
+
+
+// ========================
+//      ***** Practice Problem 2 *****
+// ======================== 
+
+const whatLove = (love) =>{
+    if(love === "Rose"){
+       return
+    }
+
+}
